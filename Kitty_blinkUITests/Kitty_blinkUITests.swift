bee795b53362935201430b59ea7665e5510f8ac1
agaisab <agaisab@gmail.com>
2021-11-15 15:50:12 +0100

    Tests

--- Kitty_blinkUITests/Kitty_blinkUITests.swift
+++ Kitty_blinkUITests/Kitty_blinkUITests.swift
@@ -9,34 +9,19 @@ import XCTest
 
 class Kitty_blinkUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    // Test, Does app loaded table. It Should equal 1 and did have 5 cells structure to confirm to API
+    func testDidTableExist() throws {
+    
         let app = XCUIApplication()
         app.launch()
+        
+        XCTAssertEqual(app.tables.count, 1)
+        XCTAssertEqual(app.tables.cells.count, 5)
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
 }
+
+
